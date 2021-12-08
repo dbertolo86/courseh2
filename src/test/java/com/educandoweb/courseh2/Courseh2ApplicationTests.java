b0cb5f58053e723026f920935ccdbaa7548f3a70
Diego Bertolo <dbertolo86@gmail.com>
2021-12-08 12:25:32 -0300

    Project Created

---
+++ src/test/java/com/educandoweb/courseh2/Courseh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Courseh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
