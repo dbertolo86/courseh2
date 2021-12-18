cdbe156e8ed47aed40e593281930289d1dc65eba
Diego Bertolo <dbertolo86@gmail.com>
2021-12-18 19:40:17 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/courseh2/Courseh2Application.java
+++ src/main/java/com/educandoweb/courseh2/Courseh2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Courseh2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Courseh2Application.class, args);
 	}
 
