5ad0d40d736e0b938c2780e8413eddf9d814e024
Diego Bertolo <dbertolo86@gmail.com>
2022-01-18 21:00:11 -0300

    Product

---
+++ src/main/java/com/educandoweb/courseh2/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.educandoweb.courseh2.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.courseh2.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
--- src/main/java/com/educandoweb/courseh2/resources/ProductResource.java
+++ src/main/java/com/educandoweb/courseh2/resources/ProductResource.java
@@ -10,28 +10,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.courseh2.entities.Product;
-import com.educandoweb.courseh2.services.ProductService;
+import com.educandoweb.courseh2.entities.Category;
+import com.educandoweb.courseh2.services.CategoryService;
 
 @RestController
-@RequestMapping(value = "/products")
-public class CategoryResource {
+@RequestMapping(value = "/categories")
+public class ProductResource {
 
 	@Autowired
-	private ProductService productService;
+	private CategoryService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll() {
+	public ResponseEntity<List<Category>> findAll() {
 		
-		List<Product> list  = productService.findall();
+		List<Category> list  = userService.findall();
 		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product p = productService.findById(id);
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category u = userService.findById(id);
 		
-		return ResponseEntity.ok().body(p);
+		return ResponseEntity.ok().body(u);
 	}
 }
