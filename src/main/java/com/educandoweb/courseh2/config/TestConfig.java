902254fbf094e3021dfe2b392dc7b72605c9b89e
Diego Bertolo <dbertolo86@gmail.com>
2022-01-26 18:19:19 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/educandoweb/courseh2/config/TestConfig.java
+++ src/main/java/com/educandoweb/courseh2/config/TestConfig.java
@@ -9,11 +9,13 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.courseh2.entities.Category;
 import com.educandoweb.courseh2.entities.Order;
+import com.educandoweb.courseh2.entities.OrderItem;
 import com.educandoweb.courseh2.entities.Product;
 import com.educandoweb.courseh2.entities.User;
 import com.educandoweb.courseh2.entities.enums.OrderStatus;
 import com.educandoweb.courseh2.repositories.UserRepository;
 import com.educandoweb.courseh2.repositories.CategoryRepository;
+import com.educandoweb.courseh2.repositories.OrderItemRepository;
 import com.educandoweb.courseh2.repositories.OrderRepository;
 import com.educandoweb.courseh2.repositories.ProductRepository;
 
@@ -27,6 +29,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
@@ -63,6 +68,15 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		
 		p1.getCategories().add(cat2);
@@ -73,6 +87,8 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+
 		
 		
 
---
+++ src/main/java/com/educandoweb/courseh2/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.courseh2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.courseh2.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
