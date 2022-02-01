c745a0e63328652b7d8e250c134e4a5aafe28331
Diego Bertolo <dbertolo86@gmail.com>
2022-02-01 15:33:14 -0300

    User update

--- src/main/java/com/educandoweb/courseh2/resources/UserResource.java
+++ src/main/java/com/educandoweb/courseh2/resources/UserResource.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.courseh2.entities.User;
+import com.educandoweb.courseh2.repositories.UserRepository;
 import com.educandoweb.courseh2.services.UserService;
 
 import java.net.URI;
@@ -59,6 +61,14 @@ public class UserResource {
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = userService.update(id, obj);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
