631e81e035da02992854562a1ce9c993bbd64147
Diego Bertolo <dbertolo86@gmail.com>
2022-02-01 15:03:44 -0300

    user insert

--- src/main/java/com/educandoweb/courseh2/resources/UserResource.java
+++ src/main/java/com/educandoweb/courseh2/resources/UserResource.java
@@ -2,16 +2,20 @@ package com.educandoweb.courseh2.resources;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.courseh2.entities.User;
 import com.educandoweb.courseh2.services.UserService;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -33,5 +37,19 @@ public class UserResource {
 		User u = userService.findById(id);
 		
 		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<User> insert (@RequestBody User obj){
+
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}").
+				buildAndExpand(obj.getId())
+				.toUri(); 
+		
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
